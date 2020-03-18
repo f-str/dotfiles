296f2879f5975b7e53b3fe92db8df8d8f9c5229d
Fload <fload2000@protonmail.com>
2020-03-18 19:28:56 +0100

    Changed polybar config

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -7,6 +7,6 @@ killall -q polybar
 while pgrep -u $UID -x polybar > /dev/null; do sleep 1; done
 
 # Launch Polybar, using defualt config location /home/florian/.config/polybar/config
-polybar example &
+polybar example2 &
 
 echo "Polybar launched..."
